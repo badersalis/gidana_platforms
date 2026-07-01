f5b7d6a1af9ca9f03b6fa85a3d6ea39d42324b93
badersalis <awesomebss39@gmail.com>
2026-07-01 15:44:30 +0000

    feat first name and lastname chat

--- internal/models/message.go
+++ internal/models/message.go
@@ -12,12 +12,12 @@ type Conversation struct {
 	UpdatedAt   time.Time      `json:"updated_at"`
 	DeletedAt   gorm.DeletedAt `gorm:"index" json:"-"`
 	PropertyID  *uint          `gorm:"index" json:"property_id,omitempty"`
-	InitiatorID uint           `gorm:"not null;index" json:"initiator_id"`
-	RecipientID uint           `gorm:"not null;index" json:"recipient_id"`
+	InitiatorID uint           `gorm:"not null;index" json:"tenant_id"`
+	RecipientID uint           `gorm:"not null;index" json:"owner_id"`
 
 	Property  *Property `gorm:"foreignKey:PropertyID" json:"property,omitempty"`
-	Initiator User      `gorm:"foreignKey:InitiatorID" json:"initiator,omitempty"`
-	Recipient User      `gorm:"foreignKey:RecipientID" json:"recipient,omitempty"`
+	Initiator User      `gorm:"foreignKey:InitiatorID" json:"tenant,omitempty"`
+	Recipient User      `gorm:"foreignKey:RecipientID" json:"owner,omitempty"`
 	Messages  []Message `gorm:"foreignKey:ConversationID;constraint:OnDelete:CASCADE" json:"messages,omitempty"`
 
 	UnreadCount int      `gorm:"-" json:"unread_count"`
